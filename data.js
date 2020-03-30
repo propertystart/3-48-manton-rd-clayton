81401bffc0f63b7d371fce071098f1a323e67997
Armen Grigorian <info@propertystart.com.au>
2020-03-31 07:36:22 +1100

    Version 2

--- data.js
+++ data.js
@@ -24,22 +24,22 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -0.18955498231983725,
-        "pitch": 0.1653642429137978,
-        "fov": 1.3219339076578436
+        "yaw": 2.264820071591103,
+        "pitch": 0.22286834590419957,
+        "fov": 1.2219229634670632
       },
       "linkHotspots": [
         {
-          "yaw": -1.1828998386217098,
-          "pitch": 0.14109723112031247,
-          "rotation": 0.7853981633974483,
+          "yaw": -1.223183930261719,
+          "pitch": 0.1294641584309808,
+          "rotation": 1.5707963267948966,
           "target": "1-kitchen"
         },
         {
-          "yaw": -0.05930715542792342,
-          "pitch": 0.31421117660860887,
+          "yaw": -0.46563118905117307,
+          "pitch": 0.056234078820168065,
           "rotation": 5.497787143782138,
-          "target": "2-bedroom"
+          "target": "2-study"
         }
       ],
       "infoHotspots": []
@@ -68,14 +68,14 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -2.5274182492569235,
-        "pitch": 0.14394882245844975,
-        "fov": 1.3219339076578436
+        "yaw": -2.8629023603809056,
+        "pitch": 0.15109800085883052,
+        "fov": 1.2219229634670632
       },
       "linkHotspots": [
         {
-          "yaw": -0.18485110153962303,
-          "pitch": 0.1922844907942327,
+          "yaw": -0.2436907739193117,
+          "pitch": 0.16662733601070023,
           "rotation": 0,
           "target": "0-entry"
         }
@@ -83,76 +83,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-bedroom",
-      "name": "Bedroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "yaw": -1.2847040689274198,
-        "pitch": 0.006833920859811826,
-        "fov": 1.3219339076578436
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.2022687441989035,
-          "pitch": 0.35299663725498576,
-          "rotation": 0.7853981633974483,
-          "target": "1-kitchen"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-bathroom",
-      "name": "Bathroom",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "yaw": 0.7171623085557215,
-        "pitch": 0.40098741133473936,
-        "fov": 1.3219339076578436
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-study",
+      "id": "2-study",
       "name": "Study",
       "levels": [
         {
@@ -175,38 +106,108 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 2.7363538449354747,
-        "pitch": 0.16416238557720675,
-        "fov": 1.3219339076578436
+        "yaw": 2.997312750397567,
+        "pitch": 0.2882847164714537,
+        "fov": 1.2219229634670632
       },
       "linkHotspots": [
         {
-          "yaw": -2.6806380061121526,
-          "pitch": 0.42636772344438434,
-          "rotation": 5.497787143782138,
-          "target": "3-bathroom"
+          "yaw": -2.715517635919058,
+          "pitch": 0.1361469147444616,
+          "rotation": 4.71238898038469,
+          "target": "4-bathroom"
         },
         {
-          "yaw": -2.4676863297913965,
-          "pitch": 0.13765348611206285,
-          "rotation": 0,
-          "target": "2-bedroom"
+          "yaw": -0.23057627008063974,
+          "pitch": 0.28682856282470404,
+          "rotation": 1.5707963267948966,
+          "target": "3-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": -1.5520941608169707,
-          "pitch": 0.23080296614387663,
-          "rotation": 3.9269908169872414,
-          "target": "1-kitchen"
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.5307295179142955,
+        "pitch": 0.1552333403109376,
+        "fov": 1.2219229634670632
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1695302650883637,
+          "pitch": 0.16197194774389345,
+          "rotation": 1.5707963267948966,
+          "target": "2-study"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.849974200618714,
+        "pitch": 0.2690254685081257,
+        "fov": 1.2294983034670632
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5012846761278063,
+          "pitch": 0.08874030468886929,
+          "rotation": 7.853981633974483,
+          "target": "2-study"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Unit 3/48-50 Manton Rd Clayton",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
